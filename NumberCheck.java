f436d374e5e911e5b99c08d6b4e6a0c96ddb07eb
Athembele A. Dladla <dladlaathembele@gmail.com>
2026-03-22 15:08:19 +0200

    conditionals

---
+++ NumberCheck.java
@@ -0,0 +1,13 @@
+public class NumberCheck {
+    public static void main(String[] args) {
+        int num = 0;
+
+        if (num > 0) {
+            System.out.println("this number is positive");
+        } else if (num == 0) {
+            System.out.println("this number is zero");
+        } else {
+            System.out.println("this number is negative");
+        }
+    }
+}
