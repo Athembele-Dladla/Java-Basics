2e956db1619dd6420b36a3266be64f45e409b246
Athembele A. Dladla <dladlaathembele@gmail.com>
2026-03-28 16:31:46 +0200

    ForLoop

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Fruits array
+        String[] fruits = {"apple", "banana", "cherry"};
+
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+
+        // Numbers array
+        int[] numbers = {1, 2, 3, 4};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
